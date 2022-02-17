df99b65be1bce68838aab43b1198558f9f971079
Vik <purvikbhatt16@gmail.com>
2022-02-17 04:03:35 +0000

    Improve check input and multiple correct letters

--- hangman.cpp
+++ hangman.cpp
@@ -6,7 +6,7 @@
 #include <ctime>
 
 int NUMBER_OF_WORDS = 5;
-char *wordArray[5];
+char *wordArray[6];
 char remainingLetters[27];
 const char *ANSWER_WORD;
 char *ANSWER_LINE;
@@ -14,12 +14,12 @@ int remainingGuesses = 8;
 int correctGuesses = 0;
 bool gameOver = false;
 char currentGuess = 'a';
-//Generate Random index
+// Generate Random index
 int randomGen(int numWords) {
     return (rand() % numWords);
 }
 
-//Remove letter from remaining guesses -C
+// Remove letter from remaining guesses -C
 void removeLetter(char c) {
     int i = 0;
 
@@ -30,12 +30,13 @@ void removeLetter(char c) {
     return;
 }
 
-//Check if letter is in word -C
+// Check if letter is in word -C
 bool isCorrect(char c) {
     int i = 0;
     bool correct = false;
     for (i; i < strlen(ANSWER_WORD); i++) {
         if (ANSWER_WORD[i] == c) {
+            correctGuesses++;
             ANSWER_LINE[i] = c;
             correct = true;
         }
@@ -43,43 +44,53 @@ bool isCorrect(char c) {
     return correct;
 }
 
-//Function to check whether input is valid -C
-bool isLetter(char c) {
-    //ascii range 65 -90 CAPS, 97 - 122 lowercase
-    if ((65 <= c <= 90)) {
-        //translate to lowercase
-        c += 32;
-        currentGuess = c;
-        return true;
-    } else if (97 <= c <= 122) {
-        currentGuess = c;
-        return true;
-    } else
-        return false;
+// Function to check whether input is valid -C
+bool isValidLetter(char c) {
+    int i = 0;
+    for (i; i < strlen(remainingLetters); i++) {
+        if (tolower(remainingLetters[i]) == c) {
+            return true;
+        }
+    }
+    return false;
+    // // ascii range 65 -90 CAPS, 97 - 122 lowercase
+    // if ((65 <= c && c <= 90))
+    // {
+    //     // translate to lowercase
+    //     c += 32;
+    //     currentGuess = c;
+    //     return true;
+    // }
+    // else if (97 <= c && c <= 122)
+    // {
+    //     currentGuess = c;
+    //     return true;
+    // }
+    // else
+    //     return false;
 }
 
-//Deal with input -C
+// Deal with input -C
 void evaluateInput(char guess) {
-    if (isLetter(guess)) {
-        remainingGuesses--;
-        //Remove from available guesses
+    if (isValidLetter(guess)) {
+        // Remove from available guesses
         removeLetter(guess);
         if (isCorrect(guess)) {
-            correctGuesses++;
-            printf("*** %dCorrect guess - '%c' IS in the word ***\n", correctGuesses, guess);
+                        printf("*** Correct guess - '%c' IS in the word ***\n", guess);
             return;
         } else {
-            //Incorrect answer
+            // Incorrect answer
+            remainingGuesses--;
             printf("!!! Wrong guess - '%c' is not in the word !!!\n", guess);
             return;
         }
     } else {
-        printf("Invalid input, please enter a letter of the alphabet\n");
+        printf("Invalid input, please enter a letter from the remaining letters:\n");
         return;
     }
 }
 
-//Remove the user's guessed letter from remaining letters -I
+// Remove the user's guessed letter from remaining letters -I
 
 bool checkGameOver() {
     if (remainingGuesses == 0) {
@@ -91,9 +102,9 @@ bool checkGameOver() {
     return gameOver;
 }
 
-//Main function -C
+// Main function -C
 int main(int argc, char const *argv[]) {
-    //Seed my random number generator
+    // Seed my random number generator
     srand((unsigned int)time(NULL));
 
     const char *ant = "ant";
@@ -101,42 +112,43 @@ int main(int argc, char const *argv[]) {
     const char *sing = "sing";
     const char *oil = "oil";
     const char *rope = "rope";
+    const char *hello = "hello";
 
-    //Initialise char arrays
+    // Initialise char arrays
     strncpy(remainingLetters, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 27);
 
-    const char *wordArray[] = {ant, job, sing, oil, rope};
+    const char *wordArray[] = {ant, job, sing, oil, rope, hello};
 
-    //Obtain random word
+    // Obtain random word
     int randomIndex = randomGen(NUMBER_OF_WORDS);
-    ANSWER_WORD = wordArray[randomIndex];
+    ANSWER_WORD = wordArray[5];
     // printf("%s\n", ANSWER_WORD);
     int numLetters = strlen(ANSWER_WORD);
 
-    //Generate answer ----
+    // Generate answer ----
     int i = 0;
     char emptyWord[numLetters + 1];
     for (i; i < numLetters; i++) {
         emptyWord[i] = '-';
     }
 
-    //NULL terminator for char array - last index needs to be '\0'
+    // NULL terminator for char array - last index needs to be '\0'
     emptyWord[numLetters] = '\0';
 
     ANSWER_LINE = emptyWord;
 
     while (!gameOver) {
         char guess = 'a';
+        printf("Available letters are: %s\n", remainingLetters);
         printf("Guess a letter for the word: %s\n", emptyWord);
         printf("You have %d wrong guesses left, and have guessed %d letters out of %d so far.\n", remainingGuesses, correctGuesses, numLetters);
-        printf("Available letters are: %s\n", remainingLetters);
         scanf("%s", &guess);
-        printf("Guess was: %c", guess);
+        printf("Guess was: %c\n", guess);
 
         evaluateInput(guess);
         if (checkGameOver()) {
             printf("GAME OVER: Word was %s\n", ANSWER_WORD);
-            //deal with game over
+            // deal with game over
             return 0;
         } else {
             continue;
